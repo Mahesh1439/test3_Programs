f7e7a3b3e2079bc6a909ff8ab26136729a7cd597
innocentvenky <167870584+innocentvenky@users.noreply.github.com>
2024-12-27 19:20:24 +0530

    Update Day3_Ex6.py
    small changes

--- Day3_Ex6.py
+++ Day3_Ex6.py
@@ -1,5 +1,5 @@
 """Example 6: Python program to print a multiplication table of a given number."""
 table_num=int(input("Enter which table you want to print"))
-for x in range(1,12+1):
+for x in range(1,11):
     res=table_num*x
     print(f"{table_num} X {x} = ",res)
